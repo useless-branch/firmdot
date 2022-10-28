6d334ad488b4deeeff66fe43004606e997b119f6
Patrick Hanner <hannerpatrick@gmail.com>
2022-10-28 17:09:09 +0200

    updated kvasir working now

---
+++ src/ApplicationConfig.hpp
@@ -0,0 +1,32 @@
+#pragma once
+#include "HWConfig.hpp"
+///
+
+#include "kvasir/Util/FaultHandler.hpp"
+#include "kvasir/Util/StackProtector.hpp"
+
+using Clock          = HW::SystickClock;
+using StackProtector = Kvasir::StackProtector<>;
+using FaultHandler   = Kvasir::Fault::Handler<HW::Fault_CleanUpAction>;
+
+struct DMAConfig {
+    static constexpr auto numberOfChannels     = 6;
+    static constexpr auto callbackFunctionSize = 64;
+};
+
+using DMA = Kvasir::DMA::DmaBase<DMAConfig>;
+
+using Uart
+  = Kvasir::UART::UartBehavior<HW::UartConfig, DMA, DMA::Channel::ch0, DMA::Priority::low, 512>;
+
+
+using Startup = Kvasir::Startup::Startup<
+  HW::ClockSettings,
+  Clock,
+  HW::ComBackend,
+  FaultHandler,
+  StackProtector,
+  HW::PinConfig,
+  DMA,
+  Uart
+  >;
--- src/main.cpp
+++ src/main.cpp
@@ -1,38 +1,57 @@
-#include "HWConfig.hpp"
+#include "ApplicationConfig.hpp"
 ///
+#include "BinaryUtility.hpp"
+#include "FlipDotMatrix.hpp"
+#include "chip/DMA.hpp"
+#include "chip/UART.hpp"
 #include "kvasir/Util/FaultHandler.hpp"
 #include "kvasir/Util/StackProtector.hpp"
 #include "remote_fmt/remote_fmt.hpp"
-#include "FlipDotMatrix.hpp"
-#include "BinaryUtility.hpp"
 //#include "cup.h"
 
-
 //#include "cmake_git_version/version.hpp"
-using Clock          = HW::SystickClock;
-using StackProtector = Kvasir::StackProtector<>;
-using FaultHandler   = Kvasir::Fault::Handler<HW::Fault_CleanUpAction>;
 
-using Startup = Kvasir::Startup::
-  Startup<HW::ClockSettings, Clock, HW::ComBackend, FaultHandler, StackProtector, HW::PinConfig>;
+namespace uc_log {
+    inline void log_assert(int line, std::string_view file) { UC_LOG_C("assert in {}:{}", file, line); }
+}   // namespace uc_log
 
 int main() {
     UC_LOG_D("{}", "Test");
     std::array<std::uint8_t, 84> matrix;
     matrix.fill(0);
-    auto next = Clock::time_point{} + std::chrono::milliseconds(400);
-    auto everySecond = Clock::time_point{} + std::chrono::seconds(1);
-    FlipDotMatrix<24,28> FDMatrix;
+
+    FlipDotMatrix<24, 28> FDMatrix;
     FDMatrix.fill(false);
-    bool Babel = false;
+
+    auto next        = Clock::time_point{} + std::chrono::milliseconds(400);
+    auto everySecond = Clock::time_point{} + std::chrono::seconds(1);
+    
+    std::array<std::byte, 2> test;
+    test.fill(std::byte{0xAB});
+
     while(true) {
         auto now = Clock::now();
-        if(now > next){
-            apply(toggle(HW::Pin::Led{}));
-            next = now + std::chrono::milliseconds(200);
+        if(now > next) {
+            UC_LOG_D("I am alive");
+            if(Uart::operationState() == Uart::OperationState::succeeded){
+                UC_LOG_D("Sending...");
+                Uart::send_nocopy(test);
+            }
+            if(Uart::operationState() == Uart::OperationState::failed){
+                UC_LOG_E("Failed to send!");
+            }
+            next = now + std::chrono::seconds(1);
         }
 
-        if(now > everySecond){
+        if(!Uart::rxbuffer_.empty()){
+            UC_LOG_D("Got messages!");
+            Uart::rxbuffer_.pop();
+            apply(toggle(HW::Pin::Led{}));
+        }
+
+
+
+        if(now > everySecond) {
 
             everySecond = now + std::chrono::milliseconds(400);
         }
